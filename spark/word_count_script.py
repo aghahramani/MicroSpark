aa99e81a2f1a87d1f91d10f11a2cdd6d1c1b5081
Ali Ghahramani <aghahramani@usfca.edu>
2015-05-06 14:47:57 -0700

    test script

---
+++ spark/word_count_script.py
@@ -0,0 +1,19 @@
+from os import listdir
+from os.path import isfile, join
+
+mypath = './Data/'
+files = [ join(mypath,f) for f in listdir(mypath) if isfile(join(mypath,f)) ]
+count_dict = {}	
+for file in files:
+	f = open(file, 'r')
+	f_read = f.readlines()
+	f.close()
+	for i in f_read:
+		k = i.split()
+		for i in k : 
+			if i not in count_dict:
+				count_dict[i] = 0
+			count_dict[i]+=1
+key_sorted = sorted(count_dict.keys())
+for i in key_sorted : 
+	print i,count_dict[i]
