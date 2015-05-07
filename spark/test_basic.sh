c8b26836aafe4aced91259725ef8d2d2617fc6a0
Ali Ghahramani <aghahramani@usfca.edu>
2015-05-07 14:24:43 -0700

    Modified the test

--- spark/test_basic.sh
+++ spark/test_basic.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-./driver.py > res_spark.txt
-python word_count_script.py > res_wordcount.txt
+echo spark run time
+time ./driver.py > res_spark.txt
+echo local run time
+time python word_count_script.py > res_wordcount.txt
+echo If no output then diff returns nothing.
+diff res_spark.txt res_wordcount.txt
